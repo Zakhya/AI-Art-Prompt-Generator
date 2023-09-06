07c60e2733e6eb574fd79ca390faed2c78352cb7
Zakhya <42898990+Zakhya@users.noreply.github.com>
2023-09-05 20:09:11 -0400

    added adjustment list that auto populates with dropdown value for fine tuning

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let recomendationSentence = document.getElementById('recommendationSentence')
+let adjustmentListContainer = document.getElementById('adjustmentListContainer')
 
 const updateFinalSentence = function(){
     recomendationSentence.innerHTML = ''
     Object.keys(currentRecomendationObject).forEach(key => {
-        if(currentRecomendationObject[key]=== '') return
+        if(currentRecomendationObject[key] === '') return
         let container = document.createElement('div')
         container.setAttribute('class', 'wordContainer')
         let label = document.createElement('p')
@@ -15,10 +16,21 @@ const updateFinalSentence = function(){
         container.appendChild(word)
         container.appendChild(label)
         recomendationSentence.appendChild(container)
-
+        
     })
     console.log(currentRecomendationObject)
     console.log('update ran')
+}
+const updateAdjustmentList = function(){
+    let inputList = adjustmentListContainer.querySelectorAll('input.adjustmentInput')
+    Object.keys(currentRecomendationObject).forEach(key => {
+        inputList.forEach(a => {
+            if(a.id === `${[key]}AdjustmentInput`){  
+                    a.value = ` ${currentRecomendationObject[key]}`
+            }
+        })
+    })
+    console.log(inputList)
 }
 
 let currentRecomendationObject = {
@@ -35,26 +47,49 @@ let currentRecomendationObject = {
 }
 
 const data = {
-    media:['Photo of', 'Picture of', 'Painting of', 'drone shot of'],
+    media:['Photo of a', 'Picture of a', 'Painting of a', 'drone shot of a'],
     adjective:[' angry', ' cute', ' fluffy'],
     subject: [' man', ' woman', ' bird', ' dog'],
-    looksLike: [' Selena Gomez', ' Chris Pratt'],
-    clothing: [' cape', ' hat', ' armor'],
+    looksLike: ['that looks like Selena Gomez', 'that looks like Chris Pratt'],
+    clothing: [' wearing a cape', ' wearing a hat', ' wearing a suit of armor'],
     action: [' fighting', ' running'],
-    scene: [' city streets', ' meadow', ' moutains', ' sunset'],
-    details: [' reflections', ' metal', ' action packed'],
-    composition: [' 8K', ' high detail', ' realistic'],
-    artistStyle: [' Dr. Suess']
+    scene: ['in the city streets,', ' in a meadow,', ' in the moutains,', ' in a sunset,'],
+    details: [' reflections,', ' metal,', ' action packed,'],
+    composition: [' 8K,', ' high detail,', ' realistic,'],
+    artistStyle: ['in the style of Dr. Suess']
 }
 
 Object.keys(data).forEach(key => {
     const dropdown = document.getElementById(`${key}`);
     dropdown.addEventListener('change', e => {
-        currentRecomendationObject[key] = e.target.value
+        if(e.target.value === 'none'){
+            currentRecomendationObject[key] = ''
+        } else {
+            currentRecomendationObject[key] = e.target.value
+        }
         console.log(currentRecomendationObject)
         updateFinalSentence()
+        updateAdjustmentList()
     })
+    // create adjustment list
+    const itemContainer = document.createElement('div')
+    itemContainer.setAttribute('class', 'adjustmentListItemContainer')
+    let itemlabel = document.createElement('label')
+    itemlabel.setAttribute('for', `${[key]}AdjustmentInput`)
+    itemlabel.setAttribute('class', `adjustmentInputLabel`)
+    itemlabel.textContent = key
+
+    let itemInput = document.createElement('input')
+    itemInput.setAttribute('id', `${[key]}AdjustmentInput`)
+    itemInput.setAttribute('class', `adjustmentInput`)
+    itemInput.setAttribute('type', 'text')
+
+    itemContainer.appendChild(itemlabel)
+    itemContainer.appendChild(itemInput)
+    adjustmentListContainer.appendChild(itemContainer)
+
     data[key].forEach(item => {
+        // populate dropdowns
         const option = document.createElement('option');
         option.value = item;
         option.textContent = item;
